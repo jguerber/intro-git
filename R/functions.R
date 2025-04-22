988fc24248431c3a6c2959b59b114410b965a3ea
Josquin Guerber <josquin.guerber@gmail.com>
2025-04-22 15:07:40 +0200

    also translate comments

--- R/functions.R
+++ R/functions.R
@@ -2,13 +2,13 @@ add_one <- function(x) {
   x + 1
 }
 
-# Exemple de modifications de Bob pour le scénario 1 : dé-commenter les lignes 6 à 9
+# Example of Bob's changes for step 1a : uncomment lines 6 to 9
 # add_two <- function(x) {
 #   x <- add_one(x)
 #   return(add_one(x))
 # }
 
-# ** A dé-commenter et commit avant de démarrer le scénario 2 **
+# ** Uncomment and commit changes before starting scenario 2 **
 # add_n <- function(x, n) {
 #
 #   for (i in 1:n) {
--- main.R
+++ main.R
@@ -2,10 +2,10 @@ source(here::here("R/functions.R"))
 
 print(add_one(12))
 
-# ** A dé-commenter avant le scénario 2 **
-# print("13 + 12 valent :")
+# ** Uncomment and commit changes before starting scenario 2 **
+# print("13 + 12 equals :")
 # print(add_n(13, 12))
 #
-# print("2025 + 2026 valent :")
+# print("2025 + 2026 equals :")
 # print(add_n(2025, 2026))
 
